94f9e6bf9a20373db9aa83e92d4de0a5f9c8adce
ahmedsalah <ahmed_salah_24@hotmail.com>
2025-01-27 10:39:43 +0200

    first

---
+++ DbCore/dbs/ClinicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DbCore.Entities.Clients;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DbCore.dbs
+{
+    public class ClinicDbContext:Db_BaseContext
+    {
+
+        public ClinicDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+    }
+   
+}
